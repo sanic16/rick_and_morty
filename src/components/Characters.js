86f97cfcac2ca5cb4427a22d8a8cbf73bf2993c3
sanic16 <201222286@ingenieria.usac.edu.gt>
2023-04-19 03:52:48 -0600

    ..

---
+++ src/components/Characters.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Characters = ({characters = []}) => {
+  return (
+    <div className='row'>
+        {
+            characters.map((item, index)=> (
+                <div key={index} className='col mb-4'>
+                    <div className='card' style={{minWidth:"200px"}}>
+                        <img src={item.image}/>
+                        <div className='card-body'>
+                            <h5 className='card-title'>
+                                {item.name}
+                            </h5>
+                            <hr/>
+                            <p>
+                                species: {item.species}
+                            </p>
+                            <p>
+                                location: {item.location.name}
+                            </p>
+                        </div>
+                    </div>
+                </div>        
+            )
+            )} 
+    </div>
+  );
+};
+
+export default Characters
